bd3c95f37a4a58f24f48853cdd6e559f046f217e
Carlos Torres <catorres@capgemini.com>
2021-01-06 12:17:35 +0100

    Weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,31 +1,37 @@
 const request = require('request');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const properties = require('./properties.json');
 
-const theUrl = 'http://api.weatherstack.com/current?access_key=e26270d2de1e16f08706c45e8bfa3666&query=Madrid&units=m';
-
-request({url: theUrl}, (error, response) => {
-    if(error === null) {
-        if(response.statusCode === 200) {
-            console.log(chalk.magenta('Request correctly done!!'));
-            console.log(JSON.parse(response.body));
-        }
-    } else {
-        console.log(chalk.red.bold(error));
-    } 
-});
-
-// Notice the json option. No need of parsing
-request({
-        url: theUrl,
-        json: true
-    },
-    (error, response) => {
-        if (error === null) {
-            if (response.statusCode === 200) {
-                console.log(chalk.magenta('Request correctly done and json-parsed!!'));
-                console.log(response.body);
+yargs
+    .command({
+        command: 'forecast',
+        describe: 'Show weather forecast of the given city',
+        builder: {
+            city: {
+                describe: 'Name of the city whose weather you want to forecast',
+                demandOption: true,
+                type: 'string'
             }
-        } else {
-            console.log(chalk.red.bold(error));
+        },
+        handler: function (argv) {
+            console.log(chalk.yellow('Forecasting weather:'));
+            console.log(chalk.green('  City: ') + argv.city);
+            console.log(chalk.magenta('  Requesting...'));
+            const theUrl = properties.url.weatherstack + '&query=' + argv.city + '&units=m';
+            request({ url: theUrl, json: true }, (error, response) => {
+                if (error === null) {
+                    if (response.statusCode === 200) {
+                        console.log(chalk.magenta('  Request correctly done and json-parsed!!'));
+                        console.log('  ' + argv.city + ' is ' + response.body.current.temperature + ' degrees and ' + response.body.current.weather_descriptions);
+                    }
+                } else {
+                    console.log(chalk.red.bold(error));
+                }
+            });
         }
-    });
+    })
+    .demandCommand(1)
+    ;
+
+yargs.parse();
---
+++ weather-app/playground/requestUtils.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const chalk = require('chalk');
+
+const theUrl = 'http://api.weatherstack.com/current?access_key=e26270d2de1e16f08706c45e8bfa3666&query=Madrid&units=m';
+
+request({ url: theUrl }, (error, response) => {
+    if (error === null) {
+        if (response.statusCode === 200) {
+            console.log(chalk.magenta('Request correctly done!!'));
+            console.log(JSON.parse(response.body));
+        }
+    } else {
+        console.log(chalk.red.bold(error));
+    }
+});
+
+// Notice the json option. No need of parsing
+request({ url: theUrl, json: true }, (error, response) => {
+    if (error === null) {
+        if (response.statusCode === 200) {
+            console.log(chalk.magenta('Request correctly done and json-parsed!!'));
+            console.log(response.body);
+        }
+    } else {
+        console.log(chalk.red.bold(error));
+    }
+});

48d6673efab66b4054c080b24f4d765b5a35365d
Carlos Torres <catorres@capgemini.com>
2021-01-07 17:17:09 +0100

    Refactoring with callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,50 +3,43 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const properties = require('./properties.json');
 
+const forecastRequestCallback = (error, response) => {
+    if (error === null) {
+        if (response.statusCode === 200) {
+            console.log(chalk.magenta('  Request correctly done and json-parsed!!'));
+            console.log('  Weather is ' + response.body.current.temperature + ' degrees and ' + response.body.current.weather_descriptions);
+        }
+    } else {
+        console.log(chalk.red.bold(error));
+    }
+};
+
+const geolocationRequestCallback = (error, response) => {
+    if (error === null) {
+        if (response.statusCode === 200) {
+            console.log(chalk.magenta('  Coordinates request correctly done and json-parsed!!'));
+            console.log('  Location is ' + response.body.features[0].center[1] + ',' + response.body.features[0].center[0]);
+            console.log(chalk.magenta('  Requesting weather forecast...'));
+            const weatherStackUrl = properties.url.weatherstack + '&query=' + response.body.features[0].center[1] + ',' + response.body.features[0].center[0] + '&units=m';
+            request({ url: weatherStackUrl, json: true }, forecastRequestCallback);
+        }
+    } else {
+        console.log(chalk.red.bold(error));
+    }
+};
+
 const forecastHandler = (argv) => {
     console.log(chalk.yellow('Forecasting weather:'));
     console.log(chalk.green('  City: ') + argv.city);
-    console.log(chalk.magenta('  Requesting...'));
-    const theUrl = properties.url.weatherstack + '&query=' + argv.city + '&units=m';
-    request({ url: theUrl, json: true }, (error, response) => {
-        if (error === null) {
-            if (response.statusCode === 200) {
-                console.log(chalk.magenta('  Request correctly done and json-parsed!!'));
-                console.log('  ' + argv.city + ' is ' + response.body.current.temperature + ' degrees and ' + response.body.current.weather_descriptions);
-            }
-        } else {
-            console.log(chalk.red.bold(error));
-        }
-    });
+    console.log(chalk.magenta('  Requesting forecast...'));
+    request({ url: properties.url.weatherstack + '&query=' + argv.city + '&units=m', json: true }, forecastRequestCallback);
 };
 
 const geolocationHandler = (argv) => {
     console.log(chalk.yellow('Geolocating city:'));
     console.log(chalk.green('  City: ') + argv.city);
     console.log(chalk.magenta('  Requesting coordinates...'));
-    const mapBoxUrl = properties.url.mapbox.replace('{location}', argv.city);
-    request({ url: mapBoxUrl, json: true }, (error, response) => {
-        if (error === null) {
-            if (response.statusCode === 200) {
-                console.log(chalk.magenta('  Coordinates request correctly done and json-parsed!!'));
-                console.log('  ' + argv.city + ' is located at ' + response.body.features[0].center[1] + ',' + response.body.features[0].center[0]);
-                console.log(chalk.magenta('  Requesting weather forecast...'));
-                const weatherStackUrl = properties.url.weatherstack + '&query=' + response.body.features[0].center[1] + ',' + response.body.features[0].center[0] + '&units=m';
-                request({ url: weatherStackUrl, json: true }, (error, response) => {
-                    if (error === null) {
-                        if (response.statusCode === 200) {
-                            console.log(chalk.magenta('  Request correctly done and json-parsed!!'));
-                            console.log('  ' + argv.city + ' is ' + response.body.current.temperature + ' degrees and ' + response.body.current.weather_descriptions);
-                        }
-                    } else {
-                        console.log(chalk.red.bold(error));
-                    }
-                });
-            }
-        } else {
-            console.log(chalk.red.bold(error));
-        }
-    });
+    request({ url: properties.url.mapbox.replace('{location}', argv.city), json: true }, geolocationRequestCallback);
 }
 
 yargs

3355a4926ee329e56dffdf849f156779031d5046
Carlos Torres <catorres@capgemini.com>
2021-01-08 13:11:46 +0100

    Serving static html content

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const chalk = require('chalk');
 const properties = require('./properties.json');
-const fs = require('fs');
+
+const publicPath = path.join(__dirname, '../public');
 
 // this creates an express web-server application
-const app = express(); 
+const app = express();
 
-app.get('', (request, response) => {
-    response.send('Hello express!!');
-});
+// this set up the path/directory of the public/static content to be served
+app.use(express.static(publicPath));
 
-app.get(properties.internalUrl.help, (request, response) => {
-    response.send('Help page');
-});
+// app.get('', (request, response) => {
+//     response.send('Hello express!!');
+// });
 
-app.get(properties.internalUrl.about, (request, response) => {
-    response.send('About page');
-});
+// app.get(properties.internalUrl.help, (request, response) => {
+//     response.send('Help page');
+// });
+
+// app.get(properties.internalUrl.about, (request, response) => {
+//     response.send('About page');
+// });
 
 app.get(properties.internalUrl.weather, (request, response) => {
-    response.send('Weather page');
+    let myJsonResponse = {
+        'myWeather': {
+            'forecast': 'This is my forecast',
+            'location': 'This is my location'
+        }
+    };
+    response.send(myJsonResponse);
 });
 
 app.get(properties.internalUrl.license, (request, response) => {
@@ -29,4 +42,7 @@ app.get(properties.internalUrl.license, (request, response) => {
 // this starts up the server at indicated port 
 app.listen(3000, () => {
     console.log(chalk.yellow('Server up and runnig !!'));
+    console.log(chalk.magenta('  Dirname: ') + __dirname);
+    console.log(chalk.magenta('  Filename: ') + __filename);
+    console.log(chalk.magenta('  Joined path: ') + publicPath);
 }); 

2718deb0702aa35d11a70302aa8fb52413a4852b
Carlos Torres <catorres@capgemini.com>
2021-01-17 12:06:05 +0100

    Trying dionamic content. Problems with view engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,25 @@
-const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const chalk = require('chalk');
+const exphbs = require('express-handlebars');
+
 const properties = require('./properties.json');
 
-const publicPath = path.join(__dirname, '../public');
+const publicStaticContentPath = path.join(__dirname, '../public');
+const publicDinamicContentPath = path.join(__dirname, '../view');
 
 // this creates an express web-server application
 const app = express();
 
+// this set up the view engine for dinamic content/templates
+app.engine('hbs', exphbs.create().engine);
+app.engine('hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', 'hbs'); // hbs == handlebars
+// this set up the directory of the dinami  c views for handlebars
+app.set('views', publicDinamicContentPath);
+
 // this set up the path/directory of the public/static content to be served
-app.use(express.static(publicPath));
+app.use(express.static(publicStaticContentPath));
 
 // app.get('', (request, response) => {
 //     response.send('Hello express!!');
@@ -24,6 +32,11 @@ app.use(express.static(publicPath));
 // app.get(properties.internalUrl.about, (request, response) => {
 //     response.send('About page');
 // });
+
+app.get('', (request, response) => {
+    // name of the item to render must match exactly the name of the file in 'view' directory
+    response.render('index'); 
+});
 
 app.get(properties.internalUrl.weather, (request, response) => {
     let myJsonResponse = {
@@ -41,8 +54,10 @@ app.get(properties.internalUrl.license, (request, response) => {
 
 // this starts up the server at indicated port 
 app.listen(3000, () => {
-    console.log(chalk.yellow('Server up and runnig !!'));
+    console.log(chalk.yellow('Server up and runnig in port 3000 !!'));
     console.log(chalk.magenta('  Dirname: ') + __dirname);
     console.log(chalk.magenta('  Filename: ') + __filename);
-    console.log(chalk.magenta('  Joined path: ') + publicPath);
+    console.log(chalk.magenta('  Public static content path: ') + publicStaticContentPath);
+    console.log(chalk.magenta('  Public dinamic content path: ') + publicDinamicContentPath);
+    
 }); 

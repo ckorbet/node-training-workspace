f1624d3ac4840b017466481e4551bdea0b3cdb5e
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-02-07 17:59:09 +0100

    Messing around fetch function

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,18 @@
+// Notice that this js is going to be execute in client side
 console.log('Client js file loaded  !!');
+
+// Notice below URL is particular fromm the course
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     console.log('Response completed !!');
+//     response.json().then((data) => {
+//         console.log(data);
+//     });
+// });
+
+const theAddress = 'Austin';
+fetch(`http://localhost:3000/weather?address=${theAddress}`).then((response) => {
+    console.log('Response completed !!');
+    response.json().then((data) => {
+        console.log(data)
+    });    
+});

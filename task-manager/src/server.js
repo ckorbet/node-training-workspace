fd34672a2bc2b9d45b73529bb8d26fa29dceebbf
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-03-19 12:18:01 +0100

    async await integrated

--- task-manager/src/server.js
+++ task-manager/src/server.js
@@ -20,61 +20,97 @@ log.info('Express app server configured for json');
 
 app.get(props.endpoints.health, (req, res) => {
     log.info(`GET request received to ${props.endpoints.health} endpoint`);
-    res.json({
-        status: 'UP'
-    });
+    res.json({ status: 'UP' });
 });
 log.info(`Health check endpoint ${props.endpoints.health} registered`);
 
-app.post(props.endpoints.users, (req, res) => {
+app.post(props.endpoints.users, async (req, res) => {
     log.info(`POST request received to ${props.endpoints.users} endpoint`);
     const userToSave = new User(req.body);
     log.info(`User to save: ${JSON.stringify(userToSave)}`);
-    userToSave.save().then(() => {
+    try {
+        await userToSave.save();
         log.info(`User correctly saved: ${JSON.stringify(userToSave)}`);
         res.json(userToSave);
-    }).catch((error) => {
+    } catch(error) {
         log.error(`Something went wrong. Review the saving: ${error}`)
         res.status(400).json({
             _message: error._message,
             name: error.name,
             message: error.message
         });
-    });       
+    }    
+    // userToSave.save().then(() => {
+    //     log.info(`User correctly saved: ${JSON.stringify(userToSave)}`);
+    //     res.json(userToSave);
+    // }).catch((error) => {
+    //     log.error(`Something went wrong. Review the saving: ${error}`)
+    //     res.status(400).json({
+    //         _message: error._message,
+    //         name: error.name,
+    //         message: error.message
+    //     });
+    // });       
 });
 log.info(`Users POST endpoint ${props.endpoints.users} registered`);
 
-app.get(props.endpoints.users, (req, res) => {
+app.get(props.endpoints.users, async (req, res) => {
     log.info(`GET request received to ${props.endpoints.users} endpoint`);
-    User.find({}).then((users) => {
+    try {
+        const users = await User.find({}); 
         log.info('Users correctly fetched');
         res.json(users);
-    }).catch((error) => {
+    } catch (error) {
         log.error(`Something went wrong. Review the fetch: ${error}`)
         res.status(500).json({
             _message: error._message,
             name: error.name,
             message: error.message
         });
-    });
+    }
+
+    // User.find({}).then((users) => {
+    //     log.info('Users correctly fetched');
+    //     res.json(users);
+    // }).catch((error) => {
+    //     log.error(`Something went wrong. Review the fetch: ${error}`)
+    //     res.status(500).json({
+    //         _message: error._message,
+    //         name: error.name,
+    //         message: error.message
+    //     });
+    // });
 });
 log.info(`Users GET endpoint ${props.endpoints.users} registered`);
 
-app.get(props.endpoints.user, (req, res) => {
+app.get(props.endpoints.user, async (req, res) => {
     log.info(`GET request received to ${props.endpoints.user} endpoint with params ${JSON.stringify(req.params)}`);
     const idToFetch = req.params.id;
     if(idToFetch) {
-        User.findById(idToFetch).then((user) => {
+        try {
+            const user = await User.findById(idToFetch);
             log.info(`User correctly fetched: ${JSON.stringify(user)}`);
             res.json(user);
-        }).catch((error) => {
+        } catch (error) {
             log.error(`Something went wrong. Review the fetch: ${error}`)
             res.status(500).json({
                 _message: error._message,
                 name: error.name,
                 message: error.message
             });
-        });
+        }
+
+        // User.findById(idToFetch).then((user) => {
+        //     log.info(`User correctly fetched: ${JSON.stringify(user)}`);
+        //     res.json(user);
+        // }).catch((error) => {
+        //     log.error(`Something went wrong. Review the fetch: ${error}`)
+        //     res.status(500).json({
+        //         _message: error._message,
+        //         name: error.name,
+        //         message: error.message
+        //     });
+        // });
     } else {
         res.status(400).json({
             message: 'Missing required id route param'
@@ -83,21 +119,34 @@ app.get(props.endpoints.user, (req, res) => {
 });
 log.info(`User GET endpoint ${props.endpoints.user} registered`);
 
-app.get(props.endpoints.task, (req, res) => {
+app.get(props.endpoints.task, async (req, res) => {
     log.info(`GET request received to ${props.endpoints.task} endpoint with params ${JSON.stringify(req.params)}`);
     const idToFetch = req.params.id;
     if(idToFetch) {
-        Task.findById(idToFetch).then((task) => {
+        try {
+            const task = await Task.findById(idToFetch);
             log.info(`Task correctly fetched: ${JSON.stringify(task)}`);
-            res.json(task);
-        }).catch((error) => {
+            res.json(task);    
+        } catch (error) {
             log.error(`Something went wrong. Review the fetch: ${error}`)
             res.status(500).json({
                 _message: error._message,
                 name: error.name,
                 message: error.message
             });
-        });
+        }        
+
+        // Task.findById(idToFetch).then((task) => {
+        //     log.info(`Task correctly fetched: ${JSON.stringify(task)}`);
+        //     res.json(task);
+        // }).catch((error) => {
+        //     log.error(`Something went wrong. Review the fetch: ${error}`)
+        //     res.status(500).json({
+        //         _message: error._message,
+        //         name: error.name,
+        //         message: error.message
+        //     });
+        // });
     } else {
         res.status(400).json({
             message: 'Missing required id route param'
@@ -106,37 +155,63 @@ app.get(props.endpoints.task, (req, res) => {
 });
 log.info(`Task GET endpoint ${props.endpoints.task} registered`);
 
-app.post(props.endpoints.tasks, (req, res) => {
+app.post(props.endpoints.tasks, async (req, res) => {
     log.info(`POST request received to ${props.endpoints.tasks} endpoint`);
     const taskToSave = new Task(req.body);
     log.info(`Task to save: ${JSON.stringify(taskToSave)}`);
-    taskToSave.save().then(() => {
+    try {
+        await taskToSave.save();
         log.info(`Task correctly saved: ${JSON.stringify(taskToSave)}`);
         res.json(taskToSave);
-    }).catch((error) => {
+    } catch (error) {
         log.error(`Something went wrong. Review the saving: ${error}`)
         res.status(400).json({
             _message: error._message,
             name: error.name,
             message: error.message
         });
-    });       
+    }
+
+    // taskToSave.save().then(() => {
+    //     log.info(`Task correctly saved: ${JSON.stringify(taskToSave)}`);
+    //     res.json(taskToSave);
+    // }).catch((error) => {
+    //     log.error(`Something went wrong. Review the saving: ${error}`)
+    //     res.status(400).json({
+    //         _message: error._message,
+    //         name: error.name,
+    //         message: error.message
+    //     });
+    // });       
 });
 log.info(`Tasks POST endpoint ${props.endpoints.tasks} registered`);
 
-app.get(props.endpoints.tasks, (req, res) => {
+app.get(props.endpoints.tasks, async (req, res) => {
     log.info(`GET request received to ${props.endpoints.tasks} endpoint`);
-    Task.find({}).then((tasks) => {
+    try {
+        const tasks = await Task.find({});
         log.info('Tasks correctly fetched');
         res.json(tasks);
-    }).catch((error) => {
+    } catch (error) {
         log.error(`Something went wrong. Review the fetch: ${error}`)
         res.status(500).json({
             _message: error._message,
             name: error.name,
             message: error.message
         });
-    });
+    }
+
+    // Task.find({}).then((tasks) => {
+    //     log.info('Tasks correctly fetched');
+    //     res.json(tasks);
+    // }).catch((error) => {
+    //     log.error(`Something went wrong. Review the fetch: ${error}`)
+    //     res.status(500).json({
+    //         _message: error._message,
+    //         name: error.name,
+    //         message: error.message
+    //     });
+    // });
 });
 log.info(`Tasks GET endpoint ${props.endpoints.tasks} registered`);
 
